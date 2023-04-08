c322ac0aaf61f1cd15885ec588618f364cbc635d
Richard Marmorstein <richardm@stripe.com>
2023-04-07 17:41:12 -0700

    js

---
+++ netlify/functions/hello.js
@@ -0,0 +1,8 @@
+exports.handler = (event, context) => {
+  return Promise.resolve.then(() => {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Hello World" }),
+    };
+  }
+}
--- netlify/functions/hello.ts
+++
@@ -1,7 +0,0 @@
-export const handler = async (event, context) => {
-  console.log(require('stripe'))
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
-  };
-}
