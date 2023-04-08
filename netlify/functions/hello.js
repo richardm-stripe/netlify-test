10544e84b1bbf75479c49d661f79678d77f9de7a
Richard Marmorstein <richardm@stripe.com>
2023-04-07 17:47:56 -0700

    go

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -2,7 +2,7 @@ exports.handler = (event, context) => {
   return Promise.resolve().then(() => {
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: "Hello World" }),
+      body: JSON.stringify({ message: require('stripe').PACKAGE_VERSION, message2: typeof require('stripe')('sk_test').checkout.sessions.listLineItems }),
     };
   });
 }
